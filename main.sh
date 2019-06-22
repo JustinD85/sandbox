ff1d673d2607d44b03b0e180ffa132f794c2c48a
Justin Duncan <justin@baal-software.com>
2019-06-22 08:06:22 -0600

    Utilizes looping

--- main.sh
+++ main.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
 #Print to command line
 MESSAGE="Scripting isn't that bad so far!"
 echo "Leveling up my scripting. $MESSAGE"
 
 sleep 1
 
+#Prints name of host machine
 HOST=$(hostname)
 echo "This script is running on $HOST"
 
 sleep 1
 
+#Checks a file on the host system
 FILE=~/etc/shadow
 
 if [ -e $FILE ]
@@ -22,4 +25,12 @@ then
         echo "You can NOT write to the shadow file"
     fi
 fi
+
+#Utilizes loops
+NAMES="man bear pig dog cat sheep"
+
+for NAME in $NAMES
+do
+    echo $NAME
+done
 
