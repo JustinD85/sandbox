2587fea799a6776e834998289694f19151100add
Justin Duncan <justin@baal-software.com>
2019-06-22 07:53:43 -0600

    Adds conditional logic for write permission to shadow file

--- main.sh
+++ main.sh
@@ -3,12 +3,23 @@
 MESSAGE="Scripting isn't that bad so far!"
 echo "Leveling up my scripting. $MESSAGE"
 
+sleep 1
+
 HOST=$(hostname)
 echo "This script is running on $HOST"
 
+sleep 1
+
 FILE=~/etc/shadow
 
-if [ -e "$FILE" ]
+if [ -e $FILE ]
 then
     echo "Shadow passwords are enabled"
+    if [ -w $FILE ]
+    then
+        echo "You can write to the shadow file"
+    else
+        echo "You can NOT write to the shadow file"
+    fi
 fi
+
