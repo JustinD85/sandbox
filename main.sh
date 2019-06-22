d524032ebe0f506b646288ee321fa980ea44a50d
Justin Duncan <justin@baal-software.com>
2019-06-22 07:31:15 -0600

    Adds prompt that reports status of shadow passwords

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #Print to command line
 MESSAGE="Scripting isn't that bad so far!"
-echo "Leveling up my scripting.$MESSAGE"
+echo "Leveling up my scripting. $MESSAGE"
 
+HOST=$(hostname)
+echo "This script is running on $HOST"
+
+FILE=~/etc/shadow
+
+if [ -e "$FILE" ]
+then
+    echo "Shadow passwords are enabled"
+fi
