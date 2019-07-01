f3b854d6d6d8180af88635174cd4825479043d7c
Justin D <justin@baal-software.com>
2019-07-01 13:03:29 -0600

    Updates handler to use util method from ring

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,14 +1,14 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as r]))
 
 (defn handler [request-map]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<html><body> your IP is: "
-              (:remote-addr request-map)
-              "<div> request has: "
-              request-map
-              "<div/></body></html>")})
+  (r/response
+   (str "<html><body> your IP is: "
+        (:remote-addr request-map)
+        "<div> reqeust-map contains: "
+        request-map
+        "<div/></body></html>")))
 
 (defn -main []
   (jetty/run-jetty
