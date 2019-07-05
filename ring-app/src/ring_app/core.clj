a299dbc55fee435b278cf21a499e4e1935ee7d9e
Justin D <justin@baal-software.com>
2019-07-05 05:29:08 -0600

    Adds ability to server pages  from resources folder

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -31,11 +31,27 @@
 (def routes
   [["/" {:get response-handler
          :post post-handler}]
-   ["/echo/:value" {:get get-with-value-handler}]])
+   ["/echo/:value" {:get get-with-value-handler}]
+   ["/api"
+    {:middleware [wrap-formats]}
+    ["/multiply"
+     {:post
+      (fn [request]
+        (r/ok {:result (str request)}))}]]])
 
 (def handler 
-  (reitit/ring-handler
-   (reitit/router routes)))
+  (reitit/routes
+   (reitit/ring-handler
+    (reitit/router routes))
+   (reitit/create-resource-handler
+    {:path "/"})
+   (reitit/create-default-handler
+    {:not-found
+     (constantly (r/not-found "404 - Page not found"))
+     :method-not-allowed
+     (constantly (r/method-not-allowed "405 - Not allowed"))
+     :not-acceptable
+     (constantly (r/not-acceptable "406 - Not acceptable"))})))
 
 (defn -main []
   (jetty/run-jetty
