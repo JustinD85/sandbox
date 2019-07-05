fe6e88381753d6f4b2fae3028b61297a7f2a8f50
Justin D <justin@baal-software.com>
2019-07-05 01:09:38 -0600

    Adds post route and grabs value from params

--- ring-app/project.clj
+++ ring-app/project.clj
@@ -4,6 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.6.3"]]
+                 [ring "1.6.3"]
+                 [metosin/ring-http-response "0.9.0"]
+                 [metosin/muuntaja "0.6.3"]
+                 [metosin/reitit "0.1.3"]]
   :main ring-app.core
   :repl-options {:init-ns ring-app.core})
--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,21 +1,41 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as r]
-            [ring.middleware.reload :refer [wrap-reload]]))
-
-(defn handler [request-map]
-  (r/response
-   (str "<html><body> your IP is: "
-        (:remote-addr request-map)
-        "<code>request-map contains this: "
-        request-map
-        "</code></body></html>")))
+            [ring.util.http-response :as r]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [muuntaja.middleware :refer [wrap-format]]
+            [reitit.ring :as reitit]))
 
 (defn wrap-nocache [handler]
   (fn [request]
     (-> request
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
+
+(defn wrap-formats [handler]
+  (wrap-format handler))
+
+(defn response-handler [request]
+  (r/ok
+   (str "<html><body> your IP is: "
+        request
+        "</body></html>")))
+
+(defn post-handler [request]
+  (r/ok
+   (str (:query-string request))))
+
+(defn get-with-value-handler [{{x :value} :path-params}]
+  (r/ok
+   (str "<p>the value is: " x "</p>")))
+
+(def routes
+  [["/" {:get response-handler
+         :post post-handler}]
+   ["/echo/:value" {:get get-with-value-handler}]])
+
+(def handler 
+  (reitit/ring-handler
+   (reitit/router routes)))
 
 (defn -main []
   (jetty/run-jetty
