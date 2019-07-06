f7d533e27bd7277beff6f4ae2e0086f9a34c99e2
Justin D <justin@baal-software.com>
2019-07-05 23:53:13 -0600

    Adds conditional rendering and custom filters for values

--- html-templating/src/html_templating/core.clj
+++ html-templating/src/html_templating/core.clj
@@ -1,7 +1,11 @@
 (ns html-templating.core
-  (:require [selmer.parser :as selmer]))
+  (:require [selmer.parser :as selmer]
+            [selmer.filters :as filters]))
 
+;; TEMPLATE EXAMPLES
+;; default location of .html files is the resources folder
 ;;Basic usage of selmer, the context maps would normally live in separate files
+
 (def hi
   (selmer/render "Hi, {{name}}" {:name "Justin"}))
 
@@ -17,3 +21,24 @@
   ;; or, setting project path so I don't have to give absolute path
   (selmer/set-resource-path! "home/falsemotive/Projects/Clojure/playground/html-templating/layouts/html/")
   (selmer/render-file "hello.html" {:name "Joe" :items (range 13)}))
+
+(def hello-with-nested-map
+  (selmer/render "<p>Hello, {{user.first-name}} {{user.last-name}}.</p>"
+                 {:user {:first-name "Joe" :last-name "Buddy"}}))
+
+;; FILTERING EXAMPLES
+;; allows for post-processing values before they are rendered (upperCase, etc)
+;; syntax {{value|modifier}}
+
+;; create custom filter: (filters/add-filter! :key modifier)
+(filters/add-filter! :empty? empty?)
+(def no-files
+(selmer/render "{% if files|empty? %} no files {% else %} there are files {% endif %}"
+               {:files []});;this is a no files example
+  )
+
+(def are-files
+(selmer/render "{% if files|empty? %} no files {% else %} there are files {% endif %}"
+               {:files ['f1 'f2 'f3]});;this is a files example
+)
+
