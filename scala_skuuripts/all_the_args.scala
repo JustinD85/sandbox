1375ddd31dc1399158da1d4da0e3ae6aa30e099f
Justin D <justin@baal-software.com>
2019-07-10 21:55:12 -0600

    Adds for to print out args

--- scala_skuuripts/all_the_args.scala
+++ scala_skuuripts/all_the_args.scala
@@ -6,3 +6,15 @@ def printArguments(arguments: Array[String]): Unit = {
 }
 
 printArguments(args)
+
+// An idiomatic way to do this explicitly would be:
+// args.foreach(arg => println(arg))
+// alt: args.foreach( (arg: String) => println(arg))
+// I am being as verbose as possible while learning
+
+// yet another way: being concise
+// args.foreach(println)
+// In the above, since the function literal ( arg => statement ) took only one arguement and
+// excecuted one statement I can write it with just the statement (println) instead
+
+
---
+++ scala_skuuripts/for_args.scala
@@ -0,0 +1,1 @@
+for(arg <- args) println(arg)
