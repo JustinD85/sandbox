3407381560678c23c90fdbf4bd80e4e9522e8167
Justin D <justin@baal-software.com>
2019-07-13 16:50:50 -0600

    Adds example for companion class/object with a singleton

---
+++ scala_skuuripts/companions.scala
@@ -0,0 +1,29 @@
+//companion class
+class A {
+  private var id: Int = 0
+  def currentId(): Int = id
+  def changeId(): Unit = {
+    id = A.incrementId()
+  }
+}
+
+// A singleton, companion object
+object A {
+  private var uniqueId: Int = 0
+  private def incrementId(): Int = {
+    uniqueId += 1
+    uniqueId
+  }
+}
+
+
+val a = new A
+val b = new A
+println("a: " + a.currentId())
+a.changeId()
+println("a: " + a.currentId())
+
+
+println("b: " + b.currentId())
+b.changeId()
+println("b: " + b.currentId())
