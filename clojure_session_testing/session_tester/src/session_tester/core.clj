af1c0712ddc38ff2961cdc45cba7c9eb41497b3b
Justin D <justin@baal-software.com>
2019-07-24 20:24:10 +0900

    Able to assoc a simple value and print it to clr

--- clojure_session_testing/session_tester/src/session_tester/core.clj
+++ clojure_session_testing/session_tester/src/session_tester/core.clj
@@ -1,7 +1,11 @@
 (ns session-tester.core
+  (:require [expiring-map.core :as em])
   (:gen-class))
 
+(def cache  (em/expiring-map 10))
+(em/assoc! cache :love "leeeeee")
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (get cache :love)))
