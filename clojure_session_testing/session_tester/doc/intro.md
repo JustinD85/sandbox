0f9a66930143fe785c393dd1f3a0950f376714a9
Justin D <justin@baal-software.com>
2019-07-24 20:13:20 +0900

    Adds initial files for session testing

---
+++ clojure_session_testing/session_tester/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to session_tester
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure_session_testing/session_tester/test/session_tester/core_test.clj
@@ -0,0 +1,7 @@
+(ns session-tester.core-test
+  (:require [clojure.test :refer :all]
+            [session-tester.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
