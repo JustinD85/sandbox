4ed70505646574bcbb2eef586cff61e1d72e8c78
Justin D <18714169+JustinD85@users.noreply.github.com>
2019-08-06 11:43:41 +0900

    Adds parts class and class companion object

---
+++ mini_programs/mechanic_shop/scala/Parts.scala
@@ -0,0 +1,10 @@
+
+
+class Parts{
+    Parts.listener
+
+}
+
+object Parts{
+    def listener():Unit = println("Listening!")
+}
--- mini_programs/mechanic_shop/scala/SpaceGame.scala
+++ mini_programs/mechanic_shop/scala/SpaceGame.scala
@@ -1,13 +1,16 @@
 // must supply name of standalone singleton object
 // must have main method in object
     //main method takes one parameter Array[String]: Unit
+//ORRR use the scala.App trait (which I will now do :) )))
 // compile files which will produce bytecode
     //scalac will take a while as it looks at jar files and stuff
     //fsc will startup a local server daemon
         //advantage you don't have to wait as long
         //shutdown daemon with fsc -shutdown
 
+import Parts._
 
-object SpaceGame {
-    def main(args: Array[String]): Unit = println("Awesome SpaceGame!")
+object SpaceGame extends App {
+    println("Awesome SpaceGame!")
+    Parts.listener
 }
