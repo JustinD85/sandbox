9f352c3f59329c2a3074f6b7cc150eded86ede91
Justin D <18714169+JustinD85@users.noreply.github.com>
2019-08-06 10:36:23 +0900

    changes logic to be more functional

--- scripts/scala_skuuripts/companions.scala
+++ scripts/scala_skuuripts/companions.scala
@@ -3,22 +3,22 @@ class A {
   private var id: Int = 0
   def currentId(): Int = id
   def changeId(): Unit = {
-    id = A.incrementId
+    A.incrementId 
+    id = A.nextId
   }
 }
 
 // A singleton, class companion object
 object A {
   private var uniqueId: Int = 0
-  private def incrementId(): Int = {
-    uniqueId += 1
-    uniqueId
-  }
-  def nextId(): Int = uniqueId
+  private def incrementId(): Unit = uniqueId += 1 //A procedure
+
+  def nextId(): Int = uniqueId //A function/method
 }
 
  val a = new A
  val b = new A
+
 println("a: " + a.currentId())
 a.changeId()
 println("a: " + a.currentId())
