ea0f13dac8fb53606e0f1d61ba397ed2988e47d5
Justin D <18714169+JustinD85@users.noreply.github.com>
2019-08-06 11:30:31 +0900

    Correct syntax of main method

--- mini_programs/mechanic_shop/scala/SpaceGame.scala
+++ mini_programs/mechanic_shop/scala/SpaceGame.scala
@@ -1,8 +1,13 @@
 // must supply name of standalone singleton object
 // must have main method in object
     //main method takes one parameter Array[String]: Unit
+// compile files which will produce bytecode
+    //scalac will take a while as it looks at jar files and stuff
+    //fsc will startup a local server daemon
+        //advantage you don't have to wait as long
+        //shutdown daemon with fsc -shutdown
 
 
 object SpaceGame {
-    def main(args: String): Unit = print("Awesome SpaceGame!")
+    def main(args: Array[String]): Unit = println("Awesome SpaceGame!")
 }
