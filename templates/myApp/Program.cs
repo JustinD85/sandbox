e9f88f8a974d724cefac7bb330e8e9a8d960bddc
Justin D <18714169+JustinD85@users.noreply.github.com>
2019-08-08 16:05:03 +0900

    dotnet console template installed

---
+++ templates/myApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey Justin, you are using .NET!");
+        }
+    }
+}
