248831bfbaf2f1b5dfe04a137ed1d758569745b5
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-10 23:52:37 +0900

    Adds survey app

---
+++ c_sharp/survey/survey/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var birth_month = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name + " and you are " + " with a birth month of " + birth_month);
+        }
+    }
+}
