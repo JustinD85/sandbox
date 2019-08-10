8ec46549e0eb165deb37ef70d8798443eb138ff9
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-11 08:23:29 +0900

    Creates Person class

---
+++ c_sharp/SchoolTracker/Person.cs
@@ -0,0 +1,13 @@
+class Person
+{
+    static public int Count;
+    public string Name;
+    public string Birthday;
+    public string Address;
+    public string Phone;
+
+    public Person()
+    {
+        Count++;
+    }
+}
