71a4f306d217fb5c88d79ef1e9625e54b8d0e44f
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-11 00:54:20 +0900

    Adds array example with foreach

---
+++ c_sharp/SchoolTracker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentGrades = new int[] { 80, 95, 12, 67, 78, 89, 23, 45, 99, 77 };
+            foreach (int grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
---
+++ c_sharp/counting_loops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace counting_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Count is {0}!", i);
+            }
+
+        }
+    }
+}
