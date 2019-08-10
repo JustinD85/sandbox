f08834defea2675a4ca5f30194129b203d17cac5
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-11 08:39:04 +0900

    Removes constructor I ma not gonna use

--- c_sharp/SchoolTracker/Student..cs
+++ c_sharp/SchoolTracker/Student..cs
@@ -1,19 +1,5 @@
 
 class Student : Person
 {
-
-    public string Grade;
-
-    public Student(string name, string grade, string birthday, string address, string phone)
-    {
-        Name = name;
-        Grade = grade;
-        Birthday = birthday;
-        Address = address;
-        Phone = phone;
-    }
-    public Student()
-    {
-
-    }
+    public int Grade;
 }
