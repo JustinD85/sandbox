4289219f7fa0837c30faa346d61e00e4b9aeecbd
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-11 07:58:38 +0900

    Adds Student class, not sure if I should use namespace

---
+++ c_sharp/SchoolTracker/Student..cs
@@ -0,0 +1,14 @@
+
+class Student
+{
+    static public int Count;
+    public string Name;
+    public string Grade;
+    public string Birthday;
+    public string Address;
+    private string phone;
+    public string Phone
+    {
+        set { phone = value; }
+    }
+}
