7d3f3654bd180c4a75cc623b1376e3328a6a7529
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-11 18:08:09 +0900

    Adds collection and thread safe examples

---
+++ c_sharp/collections/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace collections
+{
+    class Program
+    {
+        //Allows for thread safe operations
+        //one example is below where only one value with the same key is added
+        //the other is ignore, safely(sort of like a race condition)
+        static ConcurrentDictionary<string, string> ccConfig = new ConcurrentDictionary<string, string>();
+        static void Main(string[] args)
+        {
+            var customers = new List<string>();
+            customers.Add("Jill");
+            customers.Add("Bob");
+            customers.Add("Morgan");
+            WriteLine(customers.Count);
+
+            var config = new Dictionary<string, string>();
+            //No duplicates or error
+            config.Add("resolution", "1920x1080");
+            config.Add("reactjs", "16.8.1");
+            config.Add("@aspnet/signalr", "2.2.1");
+            config.Add("docker", "5.2");
+            config.Add("wsl", "^2");
+
+            foreach (var tuple in config)
+            {
+                WriteLine(tuple);
+            }
+            WriteLine(config["reactjs"]);
+
+            //Using multiple threads with dictionary
+            Thread thread1 = new Thread(new ThreadStart(AddItem));
+            Thread thread2 = new Thread(new ThreadStart(AddItem));
+            thread1.Start();
+            thread2.Start();
+        }
+
+        static void AddItem()
+        {
+            ccConfig.TryAdd("1", "2");
+            WriteLine(ccConfig.Count);
+        }
+    }
+}
