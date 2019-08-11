273b6211fdd40d2c360aafd0a8759b5c951a3429
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-11 20:44:39 +0900

    Adds example of using Thread for hacking

---
+++ c_sharp/threads/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threads
+{
+    class Program
+    {
+        static bool hacking = true;
+        static void Main(string[] args)
+        {
+            WriteLine("Simulating Hacking the Internet... ");
+            Hack();
+            WriteLine("Waiting for the Hack to Complete... ");
+
+            while (hacking)
+            { WriteLine("Waiting...:> "); ReadLine(); }
+        }
+
+        static void Hack()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3500);
+                WriteLine("Hack Complete! Watch out for the Feds... ");
+                hacking = false;
+            });
+        }
+    }
+}
