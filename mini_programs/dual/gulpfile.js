759e2f90ede8005fdc7ae42ca31d26f3a971a4d7
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-27 06:41:24 -0400

    refactor gulp procedures

--- mini_programs/dual/gulpfile.js
+++ mini_programs/dual/gulpfile.js
@@ -1,24 +1,30 @@
 const gulp = require('gulp')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
+const watchify = require('watchify')
 const tsify = require('tsify')
+const fancyLog = require('fancy-log')
 const paths = {
     pages: ['src/*.html']
 };
+
+const watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify))
+
+const bundle = () => watchedBrowserify
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'))
 
 gulp.task('copy-html', () => (
     gulp.src(paths.pages)
         .pipe(gulp.dest('dist'))))
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), () => (
-    browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
-        .plugin(tsify)
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('dist')))))
+gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle))
+watchedBrowserify.on('update', bundle)
+watchedBrowserify.on('log', fancyLog)
