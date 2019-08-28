4b042e1ab8343f17c273bba77da19b84e093ab14
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-28 03:47:35 -0400

    gulp build setup complete(for now)

--- mini_programs/dual/gulpfile.js
+++ mini_programs/dual/gulpfile.js
@@ -1,54 +1,41 @@
 const gulp = require('gulp')
 const browserify = require('browserify')//bundles into one Js file
 const vinylSource = require('vinyl-source-stream')// converts file output of browserify to something gulp understand(vinyl)
-const watchify = require('watchify')//keeps gulp running, recompliles on file change
+const buffer = require('vinyl-buffer')//convert babel output to gulp readable format
 const tsify = require('tsify')//gives access to typescript compiler
-const uglify = require('gulp-uglify')
 const sourcemaps = require('gulp-sourcemaps')
-const buffer = require('vinyl-buffer')
+const paths = { pages: ['src/*.html'] };
+const watchify = require('watchify')//keeps gulp running, recompliles on file change
+const uglify = require('gulp-uglify')//minify code
 const fancyLog = require('fancy-log')
-const paths = {
-    pages: ['src/*.html']
-};
 
-// const watchedBrowserify = watchify(browserify({
-//     basedir: '.',
-//     debug: true,
-//     entries: ['src/main.ts'],
-//     cache: {},
-//     packageCache: {}
-// }).plugin(tsify))
+const watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify))
 
-// const bundle = () => watchedBrowserify
-//     .bundle()
-//     .pipe(vinylSource('bundle.js'))
-//     .pipe(gulp.dest('dist'))
 
-// gulp.task('copy-html', () => (
-//     gulp.src(paths.pages)
-//         .pipe(gulp.dest('dist'))))
-
-// gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle))
-// watchedBrowserify.on('update', bundle)
-// watchedBrowserify.on('log', fancyLog)
+const packing = () => watchedBrowserify
+    .transform('babelify', {
+        presets: ['es2015'],
+        extensions: ['.ts']
+    })
+    .bundle()
+    .pipe(vinylSource('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'))
 
 gulp.task('copy-html', () => (
     gulp.src(paths.pages)
         .pipe(gulp.dest('dist'))))
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), () => (
-    browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
-        .plugin(tsify)
-        .bundle()
-        .pipe(vinylSource('bundle.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist')))))        
+gulp.task('default', gulp.series(gulp.parallel('copy-html'), packing))
+
+watchedBrowserify.on('update', packing)
+watchedBrowserify.on('log', fancyLog)
