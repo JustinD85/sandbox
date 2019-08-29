2bac796edcec221acd30ba4473c71b5c16f52f28
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-29 02:01:34 -0400

    Base app work with jsx and custom library

--- mini_programs/dual/gulpfile.js
+++ mini_programs/dual/gulpfile.js
@@ -2,26 +2,22 @@ const gulp = require('gulp')
 const browserify = require('browserify')//bundles into one Js file
 const vinylSource = require('vinyl-source-stream')// converts file output of browserify to something gulp understand(vinyl)
 const buffer = require('vinyl-buffer')//convert babel output to gulp readable format
-const tsify = require('tsify')//gives access to typescript compiler
 const sourcemaps = require('gulp-sourcemaps')
 const paths = { pages: ['src/*.html'] };
 const watchify = require('watchify')//keeps gulp running, recompliles on file change
 const uglify = require('gulp-uglify')//minify code
 const fancyLog = require('fancy-log')
 
-const watchedBrowserify = watchify(browserify({
+
+const _browserify = browserify({
     basedir: '.',
     debug: true,
-    entries: ['src/main.ts'],
+    entries: ['src/main.tsx'],
     cache: {},
     packageCache: {}
-}).plugin(tsify))
+}).plugin('tsify')
 
-const packing = () => watchedBrowserify
-    .transform('babelify', {
-        presets: ['es2015'],
-        extensions: ['.ts']
-    })
+const packing = () => _browserify
     .bundle()
     .pipe(vinylSource('bundle.js'))
     .pipe(buffer())
@@ -36,5 +32,5 @@ gulp.task('copy-html', () => (
 
 gulp.task('default', gulp.series(gulp.parallel('copy-html'), packing))
 
-watchedBrowserify.on('update', packing)
-watchedBrowserify.on('log', fancyLog)
+// watchedBrowserify.on('update', packing)
+// watchedBrowserify.on('log', fancyLog)
--- mini_programs/dual/src/main.tsx
+++ mini_programs/dual/src/main.tsx
@@ -1,9 +1,14 @@
 import Person from './person'
 
+function kuru(tag, attrs, ...children) {
+    return 'custom system.'
+}
+
 const HelloApp = (divName: string, name: string) => {
     const element = document.getElementById(divName);
-    const joe = Person("Joe")
-    element.innerText = `${joe.name}`
+    const joe = Person("Jody")
+
+    element.innerText = <div> {joe.name} </div>
 }
 
 HelloApp('app', "Bob");
