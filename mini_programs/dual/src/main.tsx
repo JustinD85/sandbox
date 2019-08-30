b79d6c18da341da21668c52ea1ce26be4940e582
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-30 07:25:55 -0400

    Adds ability to put jsx in array and it still renders

--- mini_programs/dual/src/main.tsx
+++ mini_programs/dual/src/main.tsx
@@ -1,24 +1,41 @@
 import Person from './person'
+import { type } from 'os';
 
 //TODO: Commit to npm package
-function kuru(tagOrNodeFunction: any, attrs: any, ...children: any[]) {
-    if (typeof tagOrNodeFunction === 'function') return tagOrNodeFunction()
-    if (typeof tagOrNodeFunction !== 'string') return
+function kuru(tagOrNodeFunction, attrs: any, ...children: any[]) {
+    if (typeof tagOrNodeFunction === 'function') return tagOrNodeFunction()//this allows for custom JSX functions(Components)
+    if (Array.isArray(children[0])) children = children[0]//this allow for jsx to be put in array and rendered still
 
     return children.reduce((element, child) => (
         child instanceof HTMLElement && element.appendChild(child),
-        typeof child === 'string' && (element.appendChild(document.createTextNode(child))),
-        element),
-        Object.assign(document.createElement(tagOrNodeFunction), attrs))
+        (typeof child === 'string' || typeof child === 'number') && (element.appendChild(document.createTextNode(String(child)))),
+        element),//returns the mutated element
+        Object.assign(document.createElement(tagOrNodeFunction), attrs))//this allows attributes on elements, ie. class,id,etc..
 }
 
 const HelloApp = (divName: string, name: string) => {
     const element = document.getElementById(divName);
-    const joe = Person("Jody")
+    const joe = Person(name)
 
     element.replaceWith(
         <div className="foo">
             <div id="probably">Does this work tho? Let's ask {joe.name}</div>
+            <section>
+                <h1>{joe.name}'s Stats</h1>
+                <section>
+                    <h2>Record</h2>
+                    <div>Wins: {joe.record.wins}</div>
+                    <div>Losses: {joe.record.losses}</div>
+                    <div>Xp: {joe.record.xp}</div>
+                    <div>Level: {joe.record.level}</div>
+                </section>
+                <section>
+                    <h2>Deck</h2>
+                    <ul>
+                        {Object.entries(joe.deck).map(([key, value]) => <li>{key} : {value}</li>)}
+                    </ul>
+                </section>
+            </section>
         </div>)
 }
 
--- mini_programs/dual/src/person.tsx
+++ mini_programs/dual/src/person.tsx
@@ -1,10 +1,15 @@
 export default (name: string) => ({
     name,
     record: {
-        level: 0,
+        level: 1,
         xp: 0,
         wins: 0,
         losses: 0,
     },
-    deck: {},
+    deck: {
+        'charizard': 2,
+        'blueface': 4,
+        'monkeyhammer': 23,
+        'elitist': 1337
+    },
 })
