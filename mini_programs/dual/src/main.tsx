f3a52af0ae3364b7d5283439efecd2410f49b8ea
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-08-30 06:19:57 -0400

    I've done the thing... React with vanilla Js

--- mini_programs/dual/src/main.tsx
+++ mini_programs/dual/src/main.tsx
@@ -1,14 +1,25 @@
 import Person from './person'
 
-function kuru(tag, attrs, ...children) {
-    return 'custom system.'
+//TODO: Commit to npm package
+function kuru(tagOrNodeFunction: any, attrs: any, ...children: any[]) {
+    if (typeof tagOrNodeFunction === 'function') return tagOrNodeFunction()
+    if (typeof tagOrNodeFunction !== 'string') return
+
+    return children.reduce((element, child) => (
+        child instanceof HTMLElement && element.appendChild(child),
+        typeof child === 'string' && (element.appendChild(document.createTextNode(child))),
+        element),
+        Object.assign(document.createElement(tagOrNodeFunction), attrs))
 }
 
 const HelloApp = (divName: string, name: string) => {
     const element = document.getElementById(divName);
     const joe = Person("Jody")
 
-    element.innerText = <div> {joe.name} </div>
+    element.replaceWith(
+        <div className="foo">
+            <div id="probably">Does this work tho? Let's ask {joe.name}</div>
+        </div>)
 }
 
 HelloApp('app', "Bob");
