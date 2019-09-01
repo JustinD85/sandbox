09e370764b42b03b283d9e47c5ecf2509134e153
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-09-01 12:18:59 -0400

    Allows arguments to be passed to Custom Components

--- mini_programs/dual/src/containers/ProfileContainer.tsx
+++ mini_programs/dual/src/containers/ProfileContainer.tsx
@@ -2,10 +2,5 @@ import { kuru } from 'kuru'
 import Profile from '../components/Profile'
 import Person from '../models/person'
 
-export default () => (
-    <Profile
-        name={Person('Kuru').name}
-        record={Person('Kuru').record}
-        deck={Person('Kuru').deck}
-    />
-)
+//Will access a data source later...
+export default () => <Profile {...Person('Mio')} />
