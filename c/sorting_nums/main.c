2ea3600e17aa1631250c22b7ad671cf7733ccf29
Justin <18714169+JustinD85@users.noreply.github.com>
2019-10-13 22:13:43 +0000

    Annotations were added to the C sorting program

--- c/sorting_nums/main.c
+++ c/sorting_nums/main.c
@@ -1,71 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE (int ) sizeof(int)
+#define SIZE (int) sizeof(int)
 
-typedef enum {FALSE = 0, TRUE} boolean;
+typedef enum {FALSE = 0, TRUE} boolean;//defined my own boolean because there is no boolean in C
+
+//Declare functions that may be invoked... definitions at bottom
 void number_checker(int*);
 void take_numbers(int*, int);
 char* sort_numbers(int*, int);
 void clear_input(){while(getchar() != '\n');}
 
 int main(){
-	int *limit = malloc(SIZE); 
-	puts("How many numbers would you like sorted?");
-	number_checker(limit);
+  int *limit = malloc(SIZE); 
+  puts("How many numbers would you like sorted?");
+  number_checker(limit);
 
-	int* numbers_array = malloc( (*limit) * SIZE);
+  int* numbers_array = malloc( (*limit) * SIZE);
+  take_numbers(numbers_array, *limit);
+  sort_numbers(numbers_array, *limit);
 
-	take_numbers(numbers_array, *limit);
-
-	sort_numbers(numbers_array, *limit);
-
-	free(limit);
-	return 0;
+  free(limit);//frees the memory address we held so dear for this program
+  return 0;
 }
 
 /*
- * this function takes a ptr and check for a numeric value
- * of note, the while(getchar....) bit clears the input so the '\n' isn't read by next call to
- * scanf
+ * continually checks the type of input entered until a number is recognized
  * */
 void number_checker(int* number)
 {
-	boolean is_number = TRUE;
+  boolean is_number = TRUE;
 
-	while(is_number)
-	{
-		puts("Enter number: ");
-		int correct = scanf("%d", number); 
+  while(is_number)
+  {
+    puts("Enter number: ");
+    int correct = scanf("%d", number); 
 
-		if(correct == EOF || correct == 0 || *number == 0)
-			puts("Please enter a number");
-		else
-			is_number = FALSE;
-	}
+    if(correct == EOF || correct == 0 || *number == 0)
+      puts("Please enter a number");
+    else
+      is_number = FALSE;
+  }
 }		
 
+/*
+ * ensures each value in the array is a numeral
+ * */
 void take_numbers(int* array_numbers, int limit)
 {
-	for(int i = 0; i < limit; i++)
-	{
-		puts("Now, enter your  next number to be sorted..");
-		number_checker(&array_numbers[i]);	
-	}
+  for(int i = 0; i < limit; i++)
+  {
+    puts("Now, enter your  next number to be sorted..");
+    number_checker(&array_numbers[i]);	
+  }
 }
 
+/*
+ * sorts then prints numbers, ascending, to output
+ * */
 char* sort_numbers(int* number_array, int limit)
 {
-	char* return_val = malloc(limit * SIZE);
+  char* return_val = malloc(limit * SIZE);
 
-	int compare(const void *p, const void *q)
-	{
-		return *(int *)p-*(int *)q;
-	}
-	qsort(number_array, limit, SIZE, compare);	
+  // the comparator
+  int compare(const void *p, const void *q)
+  {
+    //This is simply subtracting two digits and returning the difference
+    //I must  cast the type before dereferencing becuase the types come in as void...
+    return *(int *)p - *(int *)q; 
+  }
 
-	for(int i = 0; i < limit; i++)
-	{
-		printf("Index: %3d : %3d\n\n", i, number_array[i]);
-	}
+  qsort(number_array, limit, SIZE, compare);	
+
+  for(int i = 0; i < limit; i++)
+  {
+    printf("Index: %3d : %3d\n\n", i, number_array[i]);
+  }
 
 }
