94c8a81ba8749b9a0aa5a98aaca8742cb6b395b5
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-11-11 13:26:57 -0500

    Change Hello World to my name, because narcissism

--- c_sharp/unity/hello_world/HelloWorld/Assets/Scenes/HelloWorld.cs
+++ c_sharp/unity/hello_world/HelloWorld/Assets/Scenes/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello World");
+        print("Hello Justin");
     }
 
     // Update is called once per frame
