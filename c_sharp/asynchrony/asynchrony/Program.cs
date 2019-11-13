5107eded109d7660bd2f7ba4cb101fda863ad77b
Justin Duncan <justin.duncan@agencyq.com>
2019-11-13 09:19:20 -0500

    Adds webgl example

---
+++ c_sharp/asynchrony/asynchrony/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace asynchrony
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintSum(1, 2, 3);
+            PrintSum(10, 20, 30);
+            PrintSum(100, 200, 300);
+        }
+
+        static void PrintSum(int a, int b, int c)
+        {
+            Console.WriteLine("Sum: {0}", a + b + c);
+        }
+    }
+}
