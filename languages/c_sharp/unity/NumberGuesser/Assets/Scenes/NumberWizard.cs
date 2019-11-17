f3959b28f8a03c3ced4f87b78be2614c7020c86d
justind85 <18714169+JustinD85@users.noreply.github.com>
2019-11-17 17:15:21 -0500

    Adds simple logic for a number guessing game.

--- languages/c_sharp/unity/NumberGuesser/Assets/Scenes/NumberWizard.cs
+++ languages/c_sharp/unity/NumberGuesser/Assets/Scenes/NumberWizard.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int min;
+    int max;
+    int guessedNumber;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Welcome to Number Wizard, yaaaah");
-        Debug.Log("Pick a number");
-        Debug.Log("Highest Numbre you can choose is: 100");
-        Debug.Log("Lowest Number you can choose is: 1");
-
+        BeginGame(); 
     }
 
+    void Me() { }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) Debug.LogError("Key was pressed!");
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Log("Argh, I'll guess higher then...");
+            min = guessedNumber;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Log("Argh, I'll guess lower then...");
+            max = guessedNumber;
+            NextGuess();
+        }
+        else if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Log("Woohoo. I win!");
+            BeginGame();  
+        }
     }
+    void BeginGame()
+    {
+         min = 0;
+         max = 1000;
+         guessedNumber = 500;
+
+        Log("Welcome to a Number Guesser App!");
+        Log("Think of a number between 1 - 1000");
+        Log("I will guess the number and you will press up if you number is higher, down if your number is lower and enter if I guess correctly!");
+        Log($"Is your number: {guessedNumber}");
+    }
+    void Log(string msg) { Debug.Log(msg); }
+    void NextGuess() {
+        guessedNumber = (min + max) / 2;
+        Log($"Is your number: {guessedNumber}"); }
 }
