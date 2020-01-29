176aecd395c2dbf1b2e3d98d95de15597ff67a8e
Justin <18714169+JustinD85@users.noreply.github.com>
2020-01-29 18:54:53 +0000

    Update delete functionality for basic Fulcro app

---
+++ languages/clojure/fulcro/app/project.clj
@@ -0,0 +1,3 @@
+(defproject app "0.1.0-SNAPSHOT"
+  :dependencies [[cider/cider-nrepl "0.17.0-SNAPSHOT"]
+                 [refactor-nrepl "2.4.0-SNAPSHOT"]])
--- languages/clojure/fulcro/app/src/main/app/ui.cljs
+++ languages/clojure/fulcro/app/src/main/app/ui.cljs
@@ -1,30 +1,49 @@
 (ns app.ui
   (:require
+   [app.mutations :as api]
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-   [com.fulcrologic.fulcro.dom :as dom :refer [div p ul li h5 h4]]))
+   [com.fulcrologic.fulcro.dom :as dom :refer [div p ul li h4 h5]]))
 
-(defsc Person [this {:person/keys [name age]}]
+(defsc Person [this {:person/keys [id name age] :as props} {:keys [onDelete]}]
+  ;; ^ the above simply a macro that results in React-styled Js
+  ;; 3rd argument is a map that store computed values from using the comp/computed function
+
+  ;;Below sets a local db value(similar to using localStorage)
+  ;;This is viewable in Fulcro inspect
+  {:query [:person/id :person/name :person/age]
+   :ident (fn [] [:person/id (:person/id props)])
+   :initial-state (fn [{:keys [id name age] :as params}] {:person/id id :person/name name :person/age age})}
   (li
-   (h5 (str name " (age: " age ")"))))
+   (h5 (str name " (age: " age ")"))
+   (dom/button {:onClick #(onDelete id)} "X")))
 
-(def ui-person (comp/factory Person {:keyfn :person/name}))
+(def ui-person (comp/factory Person {:keyfn :person/id}))
 
-(defsc PersonList [this {:list/keys [label people]}]
-  (div
+(defsc PersonList [this {:list/keys [id label people] :as props}]
+  {:query [:list/id :list/label {:list/people (comp/get-query Person)}]
+   :ident (fn [] [:list/id (:list/id props)])
+   :initial-state (fn [{:keys [id label]}]
+                    {:list/id id
+                     :list/label label
+                     :list/people (if (= id :friends)
+                                    [(comp/get-initial-state Person {:id 1 :name "Sally" :age 32})
+                                     (comp/get-initial-state Person {:id 2 :name "Billy" :age 29})]
+                                    [(comp/get-initial-state Person {:id 3 :name "ESally" :age 23})
+                                     (comp/get-initial-state Person {:id 4 :name "EBilly" :age 92})])})}
+  (let [delete-person (fn [person-id] (comp/transact! this [(api/delete-person {:list/id id :person/id person-id})]))]
+    (div
    (h4 label)
    (ul
-    (map ui-person people))))
+    (map (fn [p] (ui-person (comp/computed p {:onDelete delete-person}))) people)))))
 
 (def ui-person-list (comp/factory PersonList))
 
-(defsc Root [this {:keys [ui/react-key]}]
-  (let [ui-data {:friends {:list/label "Friends" :list/people
-                           [{:person/name "Bobby" :person/age 28}
-                            {:person/name "Beth" :person/age 82}]}
-                 :enemies {:list/label "Enemies" :list/people
-                           [{:person/name "EBobby" :person/age 82}
-                            {:person/name "EBeth" :person/age 28}]}}]
-
-    (div
-     (ui-person-list (:friends ui-data))
-     (ui-person-list (:enemies ui-data)))))
+(defsc Root [this {:keys [friends enemies]}]
+  ;;Real magic, this is where the check if there is state and if not sets a local db, chain reaction
+  {:query [{:friends (comp/get-query PersonList)}
+           {:enemies (comp/get-query PersonList)}]
+   :initial-state (fn [params] {:friends (comp/get-initial-state PersonList {:id :friends :label "Friends"})
+                                :enemies (comp/get-initial-state PersonList {:id :enemies :label "Enemies"})})}
+      (div
+     (ui-person-list friends)
+     (ui-person-list enemies)))
