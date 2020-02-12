a3a333a94770cf5a6a0c37139ef4b45cba5c121f
Justin Duncan <=>
2020-02-12 15:56:32 -0500

    Adds skeleton Elixir setup with tests for using multiple process and GenServer

---
+++ languages/elixir/kv/lib/bucket.ex
@@ -0,0 +1,31 @@
+defmodule KV.Bucket do
+  use Agent
+
+  @doc """
+      Sets up an agent for each test
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+      Gets value from bucket by key
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts value in bucket with key and value
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  @doc """
+   Deletes value from store, returns value removed
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+end
---
+++ languages/elixir/kv/test/bucket_test.exs
@@ -0,0 +1,26 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    bucket = start_supervised!(KV.Bucket)
+
+    %{bucket: bucket}
+  end
+
+  test "the stores default value is nil", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "eggs") == nil
+  end
+
+  test "it stores values by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 3)
+
+    assert KV.Bucket.get(bucket, "eggs") == 3
+  end
+
+  test "it deletes values by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 3)
+
+    assert KV.Bucket.delete(bucket, "eggs") == 3
+    assert KV.Bucket.get(bucket, "eggs") == nil
+  end
+end
